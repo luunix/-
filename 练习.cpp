2fef91c73804295455200ab973fd5040bbbfc31f
luunix <112396297+luunix@users.noreply.github.com>
2022-10-01 12:43:35 +0800

    Add files via upload

---
+++ 练习.cpp
@@ -0,0 +1,54 @@
+#include<stdio.h>
+//#include <conio.h>
+//#include <time.h>
+//#include <easyx.h>
+//#include <mmsystem.h>
+//#pragma comment(lib,"winmm.lib")
+//#define NUM 100
+//IMAGE img_bk[2];
+//IMAGE img_meteor[2];
+//struct Meteor
+//{
+//	int x;
+//	int y;
+//	int speed;
+//};
+//struct Meteor meteor[NUM];
+//void welcome()
+//{
+//	mciSendSpring(_T("oppen./images/浪漫空气.mp3 alias bgm"), NULL, 0, NULL);
+//	settextsty(40, 0,_T("华文行楷"));
+//	setbkmode(TRANSPARENT);
+//	white(!kbhit())
+//	{
+//		cleardevice();
+//		putimage(0, 0, img_bk);
+//		settextcolor(RGB(rand() % 256, rand() % 256));
+//		outtextxy(450,20,_T("鞠婧祎，我喜欢你"));
+//		outtextxy(200, 100, _T(("爱意随风起，风止意难平"));
+//		outtextxy(200, 150, _T("我还是喜欢你，像风吹过几万里"));
+//		outtextxy(200, 200, _T("春风十里不如你，我喜欢你"));
+//		outtextxy(200, 250, _T("曾经有份真挚的感情摆在我面前"));
+//		outtextxy(200, 300, _T("春分十里，不急你嫣然一笑"));
+//		outtextxy(200, 350, _T("天清等烟雨，而我在等你"));
+//		outtextxy(200, 400, _T("因为我爱你，所以纵容你"));
+//		outtextxy(200, 450, _T("情书是抄的，爱你是真的"));
+//		outtextxy(200, 500, _T("----------------------爱你的顿开学员"));
+//		for (size_t i = 0; i, 10; i++)
+//		{
+//			setextcolor(RGB(rand() % 256, rand() % 256));
+//			outtextxy(rand()%1200,rand()%800,_T(""))
+//		}
+//
+//	}
+//}//流星表白程序
+int main()
+{
+	printf("     *\n");
+	printf("    * *\n");
+	printf("   *   *\n");
+	printf("  *  *  *\n");
+	printf(" *       *\n");
+	printf("*         *\n");
+	getchar();
+}
